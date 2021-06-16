4eb2fe0ffa2bd48eef498c49a8f72e5e4b97fa86
Mohammadaltaeab <>
2021-06-16 13:27:39 +0000

    rkr

--- pro.js
+++ pro.js
@@ -1,13 +1,24 @@
 alert("if you need more iformation write yes if not write no") ;
 var x = prompt("do you need something else");
-
+var o = 15;
+var y = 12;
 if(x == "yes"){
    document.write("<section >"+"<p >There are countless definitions of what computer programming is, but the one I use is:Programming is how you get computers to solve problems.There are two key phrases here which are important:you: without the programmer (you), the computer is useless. It does what you tell it to do.solve problems: computers are tools. They are complex tools, admittedly, but they are not mysterious or magical: they exist to make tasks easier.Computer programs (or software) are what make computers work. Without software, modern computers are just complicated machines for turning electricity into heat. It’s software on your computer that runs your operating system, browser, email, games, movie player – just about everything.Programming is a creative task: there is no right or wrong way to solve a problem, in the same way that there is no right or wrong way to paint a picture. There are choices to be made, and one way may seem better than another, but that doesn’t mean the other is wrong! With the right skills and experience, a programmer can craft software to solve an unlimited number of problems – from telling you when your next train will arrive to playing your favourite music. The possibilities are constrained only by your imagination. That’s why I love programming</p>"+"</section>")
    }
      else if (x == "no")
    {
-     alert("as you want")
+     alert("as you want");
+   }
+   
+   else {
+     console.log('this is ropot');
+     console.log(o+y,o-y,o/y);
+   }
+   var y = prompt("write print to print the artical");
+   if ( y == "print")
+   {
+     print ("hi");
    }
    else {
-     console.log('this is ropot')
+     alert("fine")
    }
